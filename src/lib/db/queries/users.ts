5ba0ec4d763180b8f7743b40550e24f074f9d2c7
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2025-06-07 09:17:31 +0200

    🏷  Update types.

--- src/lib/db/queries/users.ts
+++ src/lib/db/queries/users.ts
@@ -2,17 +2,19 @@ import { db } from "..";
 import { users } from "../schema";
 import { eq } from 'drizzle-orm';
 
-export async function createUser(name: string) {
+export type User = typeof users.$inferSelect;
+
+export async function createUser(name: string): Promise<User> {
     const [result] = await db.insert(users).values({ name: name }).returning();
     return result;
 }
 
-export async function getUsers() {
+export async function getUsers(): Promise<User[]> {
     const results = await db.select().from(users);
     return results;
 }
 
-export async function getUser(name: string) {
+export async function getUser(name: string): Promise<User | undefined> {
     const results = await db.select().from(users).where(eq(users.name, name));
     if (results.length === 0) {
         return;
@@ -20,6 +22,6 @@ export async function getUser(name: string) {
     return results[0];
 }
 
-export async function deleteUsers() {
+export async function deleteUsers(): Promise<void> {
     await db.delete(users);
 }
