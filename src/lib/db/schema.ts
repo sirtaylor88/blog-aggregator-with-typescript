23e56eced5adf05f8e9e87a447322c27ce1d33a6
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2025-06-07 09:03:49 +0200

    🗃  Add feeds table.

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,11 +1,23 @@
-import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, text } from 'drizzle-orm/pg-core';
 
-export const users = pgTable("users", {
-    id: uuid("id").primaryKey().defaultRandom().notNull(),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at")
+export const users = pgTable('users', {
+    id: uuid('id').primaryKey().defaultRandom().notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at')
         .notNull()
         .defaultNow()
         .$onUpdate(() => new Date()),
-    name: text("name").notNull().unique(),
+    name: text('name').notNull().unique(),
+});
+
+export const feeds = pgTable('feeds', {
+    id: uuid('id').primaryKey().defaultRandom().notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at')
+        .notNull()
+        .defaultNow()
+        .$onUpdate(() => new Date()),
+    name: text('name'),
+    url: text('url').unique(),
+    userId: uuid("user_id").references(() => users.id, {onDelete: 'cascade'}),
 });
